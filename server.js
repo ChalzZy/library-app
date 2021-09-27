a368282f54b41e48c2e99b3fa2f5ad3f3145e2a6
Charles Monaghan <charles@charlesmonaghan.com>
2021-09-27 16:08:41 +1300

    Added controller and route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -8,16 +10,16 @@ app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
-    var mascots = [
-      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-      { name: 'Tux', organization: "Linux", birth_year: 1996},
-      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
+    // var mascots = [
+    //   { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    //   { name: 'Tux', organization: "Linux", birth_year: 1996},
+    //   { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    // ];
+    // var tagline = "No programming concept is complete without a cute animal mascot.";
   
     res.render('pages/index', {
-      mascots: mascots,
-      tagline: tagline
+      // mascots: mascots,
+      // tagline: tagline
     });
   });
 
@@ -25,6 +27,16 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
   res.render('pages/about');
 });
+
+app.use(routes)
+
+// mongoose connection
+const dbURI = 'mongodb://localhost:27017/node-auth';
+mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+})
 
 app.listen(3000);
 console.log('Server is listening on port 3000');
